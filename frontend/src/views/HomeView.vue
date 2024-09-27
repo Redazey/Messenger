df64d8d36c0c9d061d8f3fbd14da87fad5f083a9
Reda <97jertov97@gmail.com>
2024-09-27 16:08:00 +0300

    Верстка основных компонентов мессенджера

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <MessageList />
+
+        <v-row
+            class="pa-5"
+            style="
+            z-index: 1;
+            position: sticky;
+            background-color: white;
+            width: 100%;
+            right: 0;
+            bottom:0;">
+            
+            <v-textarea
+                max-rows="5"
+                variant="solo-filled"
+                label="Write a message..."
+                row-height="15"
+                rows="1"
+                class="ma-2"
+                auto-grow
+            />
+
+            <v-btn icon="mdi-send" size="large" class="ma-2"/>
+        </v-row>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import MessageList from '@/components/MessageList.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+    name: 'HomeView',
+    components: {
+        MessageList
+    }
 }
 </script>
