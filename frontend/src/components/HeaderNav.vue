4a93504956079bcffbd2880f4fe396d0c5f057c6
Redazey <97jertov97@gmail.com>
2024-10-14 19:55:51 +0300

    add left navigation bar

--- frontend/src/components/HeaderNav.vue
+++ frontend/src/components/HeaderNav.vue
@@ -1,9 +1,7 @@
 <template>
-  <v-toolbar
-    color="cyan-lighten-1"
-    style="z-index: 2000; position: sticky; top: 0; left: 0"
-  >
-    <v-btn icon="mdi-menu" variant="text" />
+  <v-toolbar color="cyan-lighten-1" height="10%">
+    <!-- Menu button to toggle the drawer -->
+    <v-btn icon="mdi-menu" variant="text" @click="drawer = !drawer" />
 
     <v-text-field
       append-inner-icon="mdi-magnify"
@@ -23,4 +21,71 @@
       style="color: white; font-size: larger; font-weight: bolder"
     />
   </v-toolbar>
+
+  <!-- Sidebar drawer -->
+  <v-navigation-drawer v-model="drawer" app temporary pa-5>
+    <v-list-item
+      prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+      subtitle="sandra_a88@gmailcom"
+      title="Sandra Adams"
+    />
+
+    <v-divider />
+
+    <v-list style="display: flex; flex-direction: column;">
+      <v-list-item link to="settings" title="Settings">
+        <template v-slot:prepend>
+          <v-icon>mdi-cog</v-icon>
+        </template>
+      </v-list-item>
+
+      <v-list-item link @click="createChat" title="Create chat">
+        <template v-slot:prepend>
+          <v-icon>mdi-message-plus</v-icon>
+        </template>
+      </v-list-item>
+
+      <v-list-item align-content-center align-center>
+        <template v-slot:prepend>
+          <v-icon>mdi-weather-night</v-icon>
+        </template>
+        <v-switch
+          v-model="isNightMode"
+          inset
+          @change="toggleNightMode"
+        ></v-switch>
+      </v-list-item>
+    </v-list>
+
+    <v-btn text="Log out" width="80%" style="margin-left: 20px"/>
+  </v-navigation-drawer>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'ChatToolbar',
+  setup() {
+    const drawer = ref(false);
+    const isNightMode = ref(false);
+
+    const createChat = () => {
+      // Handle chat creation
+      console.log('Creating a new Chat');
+    };
+
+    const toggleNightMode = () => {
+      // Handle toggling of night mode
+      console.log('Night Mode:', isNightMode.value);
+    };
+
+    return {
+      drawer,
+      isNightMode,
+      createChat,
+      toggleNightMode,
+    };
+  },
+});
+</script>
